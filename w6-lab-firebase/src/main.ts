256e1af6aa777c9f70d7b9be83d512ead6aac84d
Eoin Concannon <101334026+EoinConcannon@users.noreply.github.com>
2024-10-25 09:02:34 +0000

    created ionic app

---
+++ w6-lab-firebase/src/main.ts
@@ -0,0 +1,17 @@
+//4/0AVG7fiRmFiGC7NDYSS4RMGd0ur51uC3z3TLsq4VRbqyEXhzzej6mVO7XbhqX7AbcLdCFdA
+//code
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+
+import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+  ],
+});
