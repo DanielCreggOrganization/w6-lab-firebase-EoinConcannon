43a05f1de19b17e14cdd51bcc86568857a3a7736
Eoin Concannon <101334026+EoinConcannon@users.noreply.github.com>
2024-10-25 09:51:21 +0000

    working on router

--- w6-lab-firebase/src/app/home/home.page.ts
+++ w6-lab-firebase/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
